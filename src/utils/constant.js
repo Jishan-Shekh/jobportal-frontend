995b812b30e7e4d3eefba6ef1714504b8010f2ec
Jishan-Shekh <shekhjishan804@gmail.com>
2026-06-08 10:01:55 +0530

    fix: update API endpoints for production

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,7 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL = "https://jobportal-backend-8u7u.onrender.com";
 
-export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
-export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
-export const APPLICATION_API_END_POINT = `${BASE_URL}/api/v1/application`;
-export const COMPANY_API_END_POINT = `${BASE_URL}/api/v1/company`;
+export const JOB_API_POST = `${BASE_URL}/api/v1/job/post`;
+export const JOB_API_GET = `${BASE_URL}/api/v1/job/get`;
+export const JOB_API_ADMIN = `${BASE_URL}/api/v1/job/getadminjobs`;
+export const JOB_API_BY_ID = (id) => `${BASE_URL}/api/v1/job/get/${id}`;
+export const JOB_API_UPDATE = (id) => `${BASE_URL}/api/v1/job/update/${id}`;
